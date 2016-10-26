f8a4151d3684016ab4ce43c69f33bbbe30c4504c
Nnn <Nnn@i.ua>
2016-10-26 20:19:36 +0300

    start page of site and od to gitignore list

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>yo-ho-ho</h1>
+        <img src="http://cdn.newsapi.com.au/image/v1/6198d9b081304b62624735014c2a0edc" style="width: 100%"/>
+    </body>
+</html>
